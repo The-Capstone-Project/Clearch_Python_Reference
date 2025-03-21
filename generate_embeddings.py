8ba962c532cfcf74c39e46d05c1590aec8390a79
Xanthium7 <akshhaykmurali@gmail.com>
2025-03-21 21:26:57 +0530

    Add script to generate embeddings from text file using Chroma and HuggingFace

---
+++ generate_embeddings.py
@@ -0,0 +1,21 @@
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
+# from langchain_community.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import TextLoader  # Changed from PyPDFLoader
+
+# Load the os.txt file instead of PDF files
+loader = TextLoader('./os.txt')
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=100)
+docs = text_splitter.split_documents(docs)
+embedding_function = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={'device': 'cpu'})
+
+
+vectorstore = Chroma.from_documents(
+    docs, embedding_function, persist_directory="./chroma_db_nccn")
+
+print(vectorstore._collection.count())
